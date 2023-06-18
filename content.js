ad0421ef16710d49df3391ebb79452e4cfe9d6ed
soso_tughushi <soso.tughushi@invent.us>
2023-06-18 22:51:22 +0400

    small changes.

--- content.js
+++ content.js
@@ -7,22 +7,39 @@ chrome.storage.sync.get(['maxTextSize'], function(result) {
   console.log("MaxTextSize = " + maxTextSize);
 });
 
+let trimmedTextTemplate;
+// Fetch trimmedTextTemplate from storage
+chrome.storage.sync.get(['trimmedTextTemplate'], function(result) {
+  trimmedTextTemplate = result.trimmedTextTemplate || "{0} \n I will provide continuation of this in next message. For now, reply with 'READ'";
+  console.log("trimmedTextTemplate = " + trimmedTextTemplate);
+});
+
 // Function to handle keydown events on textarea
 function handleKeydown(e) {
   if (e.key == 'Enter' && !e.shiftKey) {
-    let text = $(this).val();
-    let words = text.match(/\b[-?(\w+)?]+\b/gi);
-    if (words && words.length > maxTextSize) {
-      let textToSubmit = words.slice(0, maxTextSize).join(' ');
-      $(this).val(textToSubmit);
-      let textToClipboard = words.slice(maxTextSize).join(' ');
-      console.log("Text length over "+maxTextSize+"! Submitting first "+maxTextSize+" words, copying remaining words to clipboard.");
+      trimText();
+  }
+}
+
+let observer; // Declare observer here
+
+
+function trimText() {
+  let textarea = $('#prompt-textarea');
+  let text = textarea.val();
+  if (text && text.length > maxTextSize) {
+      let lastSpacePosition = text.lastIndexOf(" ", maxTextSize);
+      let textToSubmit = text.substring(0, lastSpacePosition);
+      // Apply template here
+      textToSubmit = trimmedTextTemplate.replace('{0}', textToSubmit);
+      textarea.val(textToSubmit);
+      let textToClipboard = text.substring(lastSpacePosition + 1);
+      console.log(`Text length over ${maxTextSize}! Submitting first ${maxTextSize} characters, copying remaining characters to clipboard.`);
       // Send textToSubmit to ChatGPT API
       navigator.clipboard.writeText(textToClipboard);
-    } else {
-      console.log("Word count:", words ? words.length : 0);
+  } else {
+      console.log("Character count:", text ? text.length : 0);
       // Send text to ChatGPT API
-    }
   }
 }
 
@@ -32,11 +49,21 @@ function handleMutations(mutations) {
   if (textarea.length) {
     console.log('Textarea found:', textarea);
     textarea.on('keydown', handleKeydown);
+
+    // Get the parent div of the textarea and find the button inside it
+    let button = textarea.parent().find('button');
+    if (button.length) {
+      console.log('Button found:', button);
+      button.on('mousedown', function(e) {
+        setTimeout(trimText, 0);
+      });
+    }
+
     this.disconnect();  // stop observing once we've found the textarea and set up the event listener
   }
 }
 
 $(document).ready(function() {
-  let observer = new MutationObserver(handleMutations);
+  observer = new MutationObserver(handleMutations); // Now observer is defined before we use it
   observer.observe(document.body, { childList: true, subtree: true });  // observe changes to the body and its descendants
 });
--- options.js
+++ options.js
@@ -1,7 +1,21 @@
-document.getElementById('settings-form').onsubmit = function(e){
-    e.preventDefault();
-    let maxTextSize = document.getElementById('maxTextSize').value;
-    chrome.storage.sync.set({'maxTextSize': maxTextSize}, function() {
-        console.log('Settings saved');
+document.addEventListener("DOMContentLoaded", function() {
+    // Load values from storage and set them in the form inputs
+    chrome.storage.sync.get(['maxTextSize', 'trimmedTextTemplate'], function(result) {
+      document.getElementById('maxTextSize').value = result.maxTextSize || '';
+      document.getElementById('trimmedTextTemplate').value = result.trimmedTextTemplate || '';
     });
-}
+  });
+  
+  document.getElementById('settings-form').onsubmit = function(e) {
+    e.preventDefault();
+  
+    // Get values from form inputs
+    let maxTextSize = document.getElementById('maxTextSize').value;
+    let trimmedTextTemplate = document.getElementById('trimmedTextTemplate').value;
+  
+    // Save values to storage
+    chrome.storage.sync.set({'maxTextSize': maxTextSize, 'trimmedTextTemplate': trimmedTextTemplate}, function() {
+      console.log('Settings saved');
+    });
+  };
+  
